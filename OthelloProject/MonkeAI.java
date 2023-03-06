b7748fe5ad08e5b8e11bd70190e91c9f88fbf4de
chris <crco@itu.dk>
2023-03-06 17:36:05 +0100

    monke

--- OthelloProject/MonkeAI.java
+++ OthelloProject/MonkeAI.java
@@ -1,11 +1,6 @@
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
-/**
- * A simple OthelloAI-implementation. The method to decide the next move just
- * returns the first legal move that it finds. 
- * @author Mai Ajspur
- * @version 9.2.2018
- */
+
 public class MonkeAI implements IOthelloAI{
 
 	/**
@@ -13,11 +8,10 @@ public class MonkeAI implements IOthelloAI{
 	 */
 	public Position decideMove(GameState s){
 		ArrayList<Position> moves = s.legalMoves();
-        var monke = ThreadLocalRandom.current().nextInt(0, moves.size());
-		if ( !moves.isEmpty() )
-            
-			return moves.get(monke);
-		else
+        var monkeMove = ThreadLocalRandom.current().nextInt(0, moves.size());
+		if ( !moves.isEmpty() ){
+			return moves.get(monkeMove);
+        }else
 			return new Position(-1,-1);
 	}
 	
