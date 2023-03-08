3a75701205d72a54f24b4712851a4bd0bad31533
chris <crco@itu.dk>
2023-03-08 23:23:32 +0100

    Increases Search Depth over time + changes eval

--- OthelloProject/JohnOthello.java
+++ OthelloProject/JohnOthello.java
@@ -8,7 +8,20 @@ public class JohnOthello implements IOthelloAI {
 
 	public Position decideMove(GameState s) {
 		GameState state = s;
-		searchDepth = 2;
+		searchDepth = 3;	
+		
+		if(s.countTokens()[1]+s.countTokens()[0] >= 20){
+			searchDepth++;
+			System.out.println("U P G R A D E :" + searchDepth);
+			if(s.countTokens()[1]+s.countTokens()[0] >= 30){
+				searchDepth++;
+				System.out.println("I N S T A L L :" + searchDepth);
+				if(s.countTokens()[1]+s.countTokens()[0] >= 40){
+					searchDepth+=3;
+					System.out.println("E N H A N C E :" + searchDepth);
+				}
+			}
+		}
 		/*
 		 * if (this.firstMove = false){
 		 * this.topOfGameTree = new GameTree(s, null);
@@ -16,16 +29,17 @@ public class JohnOthello implements IOthelloAI {
 		 * this.firstMove = true;
 		 * }
 		 */
-
+		
 		ArrayList<Position> moves = state.legalMoves();
-
-		if (!moves.isEmpty())
+		if (!moves.isEmpty()){
 			return MinimaxSearch(s);
-		else
+		}
 			return new Position(-1, -1);
+		
 	}
 
 	public Position MinimaxSearch(GameState s) {
+		var startTime = System.nanoTime();
 		var currentPlayer = s.getPlayerInTurn();
 		if (currentPlayer == 1) {
 			var x = MaxValue(s, 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
@@ -33,13 +47,15 @@ public class JohnOthello implements IOthelloAI {
 			System.out.println("I was!");
 			System.out.println(s.getPlayerInTurn());
 			System.out.println(x.getPos());
+			System.out.println((System.nanoTime()-startTime)/1_000_000_000.0);
 			return x.getPos();
 		} else if (currentPlayer == 2) {
-			var y = MaxValue(s, 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+			var y = MinValue(s, 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
 			System.out.println("hewwo :3333");
 			System.out.println("I was!");
 			System.out.println(s.getPlayerInTurn());
 			System.out.println(y.getPos());
+			System.out.println((System.nanoTime()-startTime)/1_000_000_000.0);
 			return y.getPos();
 		}
 		return new Position(-1, -1);
@@ -64,9 +80,8 @@ public class JohnOthello implements IOthelloAI {
 				trueAlpha = Max(beta.getUtil(), trueAlpha);
 
 			}
-			if (counter > searchDepth) {
-				System.out.println("OWO");
-				if (beta.getUtil() >= trueBeta)
+			if (counter >= searchDepth) {
+				if (beta.getUtil() > trueBeta)
 					return beta;
 			}
 		}
@@ -92,9 +107,8 @@ public class JohnOthello implements IOthelloAI {
 				trueBeta = Min(trueBeta, alpha.getUtil());
 
 			}
-			if (counter > searchDepth) {
-				System.out.println("OWO");
-				if (alpha.getUtil() <= trueBeta)
+			if (counter >= searchDepth) {
+				if (alpha.getUtil() < trueBeta)
 					return alpha;
 			}
 		}
@@ -119,43 +133,44 @@ public class JohnOthello implements IOthelloAI {
 
 	public int calculateUtility(GameState s) {
 		// Positive values are good for black, negative values are good for white
-		System.out.println("uwu");
 		var board = s.getBoard();
 		int tokens1 = 0;
 		int tokens2 = 0;
 		var size = board.length;
-
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-
-				if (board[i][j] == 1)
-
-					if (((i == 0 && j == 0) || (i == 0 && j == size - 1) || (i == size - 1 && j == 0)
-							|| (i == size - 1 && j == size - 1)) || (i==0 && (j!=1 || j!=size-2) || i==size-1 && (j!=1 || j!=size-2))) {
-							//corners
-						tokens1 += 150;
+		if(searchDepth<=5){
+		//Board position evaluation
+			for (int i = 0; i < size; i++) {
+				for (int j = 0; j < size; j++) {
+					if (board[i][j] == 1)
+						if (((i == 0 && j == 0) || (i == 0 && j == size - 1) || (i == size - 1 && j == 0)
+					|| (i == size - 1 && j == size - 1)) || (i==0 && (j!=1 || j!=size-2) || i==size-1 && (j!=1 || j!=size-2))) {
+					//Corners
+					tokens1 += 150;
 					} else if ((i == 1) || (i == size - 2) || (j == 1) || (j == size - 2)) {
-						//Danger zone
-						tokens1 += 5;
-							
+					//Danger zone
+					tokens1 += 5;	
 					} else {
-							//Center
-						tokens1 += 10;
+					//Center
+					tokens1 += 10;
 					}
 					else if (board[i][j] == 2)
 					if (((i == 0 && j == 0) || (i == 0 && j == size - 1) || (i == size - 1 && j == 0)
-							|| (i == size - 1 && j == size - 1)) || (i==0 && (j!=1 || j!=size-2) || i==size-1 && (j!=1 || j!=size-2))) {
+					|| (i == size - 1 && j == size - 1)) || (i==0 && (j!=1 || j!=size-2) || i==size-1 && (j!=1 || j!=size-2))) {
 						tokens2 += 150;
 					} else if ((i == 1) || (i == size - 2) || (j == 1) || (j == size - 2)) {
 						tokens2 += 5;
 					} else {
 						tokens2 += 10;
 					}
-
+				}
 			}
+		}else{
+			//pure token amount evaluation
+			tokens2 = s.countTokens()[1];
+			tokens1 = s.countTokens()[0];
 		}
+		
 		int totalValue = tokens1 - tokens2;
-		System.out.println(totalValue);
 		return totalValue;
 	}
 
